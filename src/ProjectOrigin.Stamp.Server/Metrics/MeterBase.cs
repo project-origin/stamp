b6ebab7b9084156dc4839d27e42145f1ed7b040c
sahma19 <saiuan@msn.com>
2025-01-13 13:27:09 +0100

    remove dependency on IConfiguration

--- src/ProjectOrigin.Stamp.Server/Metrics/MeterBase.cs
+++ src/ProjectOrigin.Stamp.Server/Metrics/MeterBase.cs
@@ -1,10 +1,9 @@
 using System.Diagnostics.Metrics;
-using Microsoft.Extensions.Configuration;
 
 namespace ProjectOrigin.Stamp.Server.Metrics;
 
-public class MeterBase(IMeterFactory meterFactory, IConfiguration configuration)
+public class MeterBase(IMeterFactory meterFactory)
 {
     public const string MeterName = "ProjectOrigin.Stamp";
-    public Meter Meter { get; } = meterFactory.Create(configuration["StampMeterName"] ?? MeterName);
+    public Meter Meter { get; } = meterFactory.Create(MeterName);
 }
