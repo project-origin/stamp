24bae7bd7768394402f09bde9771ce1a4b09b8b2
Christoffer Frydkjær <christofferfk@gmail.com>
2025-01-15 09:25:40 +0100

    Add label (#194)
    * add label
    
    * fix

--- src/ProjectOrigin.Stamp.Test/CertificateIssuingTests.cs
+++ src/ProjectOrigin.Stamp.Test/CertificateIssuingTests.cs
@@ -228,6 +228,7 @@ public class CertificateIssuingTests : IDisposable
         var issuedMeasurements = issuedCollector.GetMeasurementSnapshot();
         var intentsMeasurements = intentsCollector.GetMeasurementSnapshot();
 
+        issuedMeasurements[0].Tags["CertificateType"].Should().Be("Production");
         Assert.Equal(1, issuedMeasurements.EvaluateAsCounter());
         Assert.Equal(1, intentsMeasurements.EvaluateAsCounter());
     }
