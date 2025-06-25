886fa26011c39e49157c78fc6f3d98bed9ed53c9
sahma19 <55732914+sahma19@users.noreply.github.com>
2025-06-25 12:15:56 +0200

    Harden Stamp dockerfile (#233)
    * Harden dockerfile

--- src/ProjectOrigin.Stamp.Server/Extensions/IServiceCollectionExtensions.cs
+++ src/ProjectOrigin.Stamp.Server/Extensions/IServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
+using System;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.IdentityModel.Protocols.Configuration;
 using ProjectOrigin.Stamp.Server.Database;
 using ProjectOrigin.Stamp.Server.Database.Postgres;
@@ -8,6 +10,8 @@ namespace ProjectOrigin.Stamp.Server.Extensions;
 
 public static class IServiceCollectionExtensions
 {
+    private static readonly string[] PostgresHealthCheckTags = ["ready", "db"];
+
     public static void ConfigurePersistance(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddSingleton<IRepositoryUpgrader, PostgresUpgrader>();
@@ -16,5 +20,8 @@ public static class IServiceCollectionExtensions
                 ?? throw new InvalidConfigurationException("Configuration does not contain a connection string named 'Database'."))
             .ValidateDataAnnotations()
             .ValidateOnStart();
+
+        var healthChecks = services.AddHealthChecks().AddCheck("self", () => HealthCheckResult.Healthy());
+        healthChecks.AddNpgSql(configuration.GetConnectionString("Database") ?? throw new InvalidOperationException(), name: "postgres", tags: PostgresHealthCheckTags);
     }
 }
